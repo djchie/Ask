0a01daf98a24986532d76289fa3b962359086e30
Derrick J Chie <djchie.dev@gmail.com>
2014-04-05 00:15:26 -0700

    Added navigation view controllers for menu
    Added navigation view controllers for each of the menu buttons

---
+++ Ask/FriendsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendsTableViewController.h
+//  Ask
+//
+//  Created by Derrick J Chie on 4/5/14.
+//  Copyright (c) 2014 Derrick J Chie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsTableViewController : UITableViewController
+
+@end
