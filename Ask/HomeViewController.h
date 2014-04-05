49876db44f7be373240b547fd83981fe48246a36
Derrick J Chie <djchie.dev@gmail.com>
2014-04-05 02:35:54 -0700

    Made connections on HomeViewController
    Connected properties on HomeViewController

--- Ask/HomeViewController.h
+++ Ask/HomeViewController.h
@@ -8,13 +8,24 @@
 
 #import <UIKit/UIKit.h>
 #import "MenuViewController.h"
-@interface HomeViewController : UIViewController 
+@interface HomeViewController : UIViewController
 {
 }
 
 @property (strong, nonatomic) IBOutlet UIView* loginView;
+@property (weak, nonatomic) IBOutlet UITableView *myQuestionsTableView;
+@property (weak, nonatomic) IBOutlet UITableView *friendsQuestionTableView;
+
+@property (weak, nonatomic) IBOutlet UIButton *askButton;
+@property (weak, nonatomic) IBOutlet UIButton *yesButton;
+@property (weak, nonatomic) IBOutlet UIButton *noButton;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tableViewSegmentedControl;
 
 - (IBAction)loginButtonTouchHandler:(id)sender;
+- (IBAction)yesButtonPressed:(id)sender;
+- (IBAction)noButtonPressed:(id)sender;
+
+
 -(MenuViewController *)getMenuViewController;
 
 @end
